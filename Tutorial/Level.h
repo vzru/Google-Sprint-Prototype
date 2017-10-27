ec2768a1013d2464a8411c82283d81740477a4dc
Kyle Disante <100617178@oncampus.local>
2017-10-27 14:32:31 -0400

    Create Level.cpp and .h
    Create a Level and a LevelMesh class, to load in a level and retrive information from them that can be used for collision.

---
+++ Tutorial/Level.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Mesh.h"
+#include "GameObject.h"
+#include <vector>
+#include "glm/vec3.hpp"
+
+class LevelMesh : public Mesh {
+public:
+	LevelMesh();
+	~LevelMesh();
+
+	// Load a mesh and send it to OpenGL and calculate the hitboxes
+	bool loadFromFile(const std::string &file);
+private:
+	std::vector<glm::vec2> ceiling;
+};
+
+class Level : GameObject {
+public:
+	Level();
+	~Level();
+
+	void update(float dt);
+	virtual void draw(ShaderProgram &shader, glm::mat4 &cameraTransform, glm::mat4 &cameraProjection);
+};
